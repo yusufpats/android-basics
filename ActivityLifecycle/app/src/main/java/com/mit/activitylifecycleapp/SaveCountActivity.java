693c4b4664aafa93c374950aea69d31005459b7c
Yusuf I Patrawala <yusufpats@gmail.com>
2021-04-17 14:30:30 +0530

    Add Android project to demonstrate ActivityLifecycle

---
+++ ActivityLifecycle/app/src/main/java/com/mit/activitylifecycleapp/SaveCountActivity.java
@@ -0,0 +1,14 @@
+package com.mit.activitylifecycleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaveCountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_count);
+    }
+}
